cf1e33181a2320f1b605c16d6f8a3c527fb1b015
patelvini <patelvini136@gmail.com>
2020-03-25 17:09:53 +0530

    Updated

--- Case_Studies/KNN/KNN_with_iris_dataset/Inbuilt/KNN_classifier.py
+++ Case_Studies/KNN/KNN_with_iris_dataset/Inbuilt/KNN_classifier.py
@@ -8,13 +8,19 @@ all_inputs = iris.data
 
 target_lables = iris.target
 
+# you can choose the number k as you want
+
+# if we are not providing the value for n_neighbors then bydefault the value for k is 5.
+
 KNN = KNeighborsClassifier(n_neighbors = 3)
 
 
 (train_inputs, test_inputs, train_classes, test_classes) = train_test_split(all_inputs, target_lables, train_size=0.7, random_state=1)
 
+print('*'*50,"Train classes",'*'*50)
 print(train_classes)
 
+print('*'*50,"Test classes",'*'*50)
 print(test_classes)
 
 KNN.fit(train_inputs,train_classes)
