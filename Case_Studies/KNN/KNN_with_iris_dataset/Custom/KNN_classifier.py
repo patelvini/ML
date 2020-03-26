3a05cda5cdb55801e2ea79688fd72712d1ba7f37
patelvini <patelvini136@gmail.com>
2020-03-26 12:04:39 +0530

    Add : Added .ipynb file for KNN Algorithm

--- Case_Studies/KNN/KNN_with_iris_dataset/Custom/KNN_classifier.py
+++ Case_Studies/KNN/KNN_with_iris_dataset/Custom/KNN_classifier.py
@@ -5,146 +5,108 @@ import os
 import operator
 from sklearn.datasets import load_iris
 import random
-
+import Commands as cmd
 
 class KNNClassifier:
 
 	def train_test_split(self,dataset):
-
 		training_data = dataset.iloc[:70].reset_index(drop = True)
-
 		testing_data = dataset.iloc[70:].reset_index(drop = True)
+		trainingSet = []
+		test_classes = []
+		test_data = []
+		for index, rows in training_data.iterrows():
+			my_list = [rows.sepal_length, rows.sepal_width, rows.petal_length, rows.petal_width, rows.species]
+			trainingSet.append(my_list)
 
-		return training_data, testing_data
+		for index, rows in testing_data.iterrows():
+			my_list = [rows.sepal_length, rows.sepal_width, rows.petal_length, rows.petal_width]
+			test_classes.append(rows.species)
+			test_data.append(my_list)
+		return trainingSet,test_data,test_classes
 
 	
 	def euclidian_distance(self, data1, data2, length):
-
 		distance = 0
-
 		for i in range(length):
-
 			distance += ((data1[i] - data2[i])**2)
 		distance = (distance ** (1/2))
-
 		return distance
 
 	def getKNeighbors(self, trainingSet, testInstance, k):
-
 		distances = []
-
 		length = len(testInstance)
-
 		for i in range(len(trainingSet)):
-
 			dist = self.euclidian_distance(testInstance, trainingSet[i], length)
-
 			distances.append((trainingSet[i], dist))
-
 		distances.sort(key = operator.itemgetter(1))
-
 		neighbors = []
-
 		for i in range(k):
 			neighbors.append(distances[i][0])
-
 		return neighbors
-
 
 	def fit(self, train_data):
 
-		trainingSet = []
-
-		for index, rows in train_data.iterrows():
-			my_list = [rows.sepal_length, rows.sepal_width, rows.petal_length, rows.petal_width, rows.species]
-
-			trainingSet.append(my_list)
-
+		trainingSet = train_data
 		return trainingSet
 
 	def predict(self, neighbors):
-
 		classVotes = {}
-
 		for i in range(len(neighbors)):
-
 			response = neighbors[i][-1]
-
 			if response in classVotes:
 				classVotes[response] +=1
 			else:
 				classVotes[response] = 1
-
 		sortedVotes = sorted(classVotes.items(), key = operator.itemgetter(1), reverse = True)
-
 		return sortedVotes[0][0]
 
 	def getAccuracy(self, testSet, predictions):
-
 		correct = 0
-
 		for i in range(len(testSet)):
 			if testSet[i] is predictions[i]:
 				correct += 1
-
 		accuracy = correct/float(len(testSet)) * 100.0
-
 		return accuracy
 
 if __name__ == '__main__':
 	
 	path = os.getcwd()
-
 	for i in range(3):
 		path = os.path.dirname(path)
-
 	data = pd.read_csv( path + '/Datasets/IRIS.csv')
-
 	data.sort_values(by = 'sepal_width', inplace = True)
-
 	KNN = KNNClassifier()
-
-	training_data =  KNN.train_test_split(data)[0]
-	testing_data = KNN.train_test_split(data)[1]
-
-	test_classes = []
-	test_data = []
-
-	for index, rows in testing_data.iterrows():
-		my_list = [rows.sepal_length, rows.sepal_width, rows.petal_length, rows.petal_width]
-		test_classes.append(rows.species)
-		test_data.append(my_list)
-
-	trainSet = KNN.fit(training_data)
-
-	k = int(input(" Enter value for the k : "))
-
-	predictions = []
-
-	for i in range(len(test_data)):
-		neighbors = KNN.getKNeighbors(trainSet, test_data[i], k)
-
-		predictions.append(KNN.predict(neighbors))
-
-		print(neighbors)
-		print(predictions)
-		print("*"*50)
-
-
-	accuracy = KNN.getAccuracy(test_classes, predictions)
-
-	print("\n Accuracy = ", accuracy, "%")
-
-	testInstance = [5.1,3.3,1.4,0.21]
-
-	neighbors = KNN.getKNeighbors(trainSet, testInstance, k)
-
-	print(" Nearest Neighbors = ")
-
-	for neighbor in neighbors:
-		print(neighbor)
-
-
-	prediction = KNN.predict(neighbors)
-
-	print("\n Prediction for test input : ",prediction)
+	training_data, testing_data, test_classes =  KNN.train_test_split(data)
+	k = 3
+	
+	
+	if cmd.args.train:
+		trainSet = KNN.fit(training_data)
+		print("Model trained Successfully !!!")
+	elif cmd.args.test:
+		predictions = []
+		trainSet = KNN.fit(training_data)
+		for i in range(len(testing_data)):
+			neighbors = KNN.getKNeighbors(trainSet, testing_data[i], k)
+			predictions.append(KNN.predict(neighbors))
+		accuracy = KNN.getAccuracy(test_classes, predictions)
+		print("\nAccuracy = ", accuracy, "%")
+	elif cmd.args.predict:
+		trainSet = KNN.fit(training_data)
+		testInstance = [cmd.args.sepal_length,cmd.args.sepal_width,cmd.args.petal_length,cmd.args.petal_width]
+		neighbors = KNN.getKNeighbors(trainSet, testInstance, k)
+		print("Nearest Neighbors = ")
+		for neighbor in neighbors:
+			print(neighbor)
+		prediction = KNN.predict(neighbors)
+		print("\nPrediction for test input : ",prediction)
+	else:
+		print("No argument given !!!")
+		print(" --train for training.")
+		print(" --test for check the accuracy.")
+		print(" --predict for predict label for given input")
+		print(" -sl for input sepal_length.")
+		print(" -sw for input sepal_width.")
+		print(" -pl for input petal_length.")
+		print(" -pw for input petal_width.")
